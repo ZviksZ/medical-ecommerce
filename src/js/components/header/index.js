aae3488e0497435465b0bd0dadd99c9fcd1ac4d2
Nikita Kapshtyk <kapshtyk.n@globalsolutions.ru>
2020-10-06 11:36:36 +0300

    каталог - адаптив

---
+++ src/js/components/header/index.js
@@ -0,0 +1,33 @@
+export class Header {
+    constructor() {
+        this.$header = document.getElementById('header');
+        this.headerClassName = 'header-color';
+
+        this.init();
+    }
+
+    init = () => {
+        this.refreshStateHeader();
+
+        window.addEventListener('scroll', () => {
+            this.refreshStateHeader();
+        });
+    }
+
+    refreshStateHeader = () => {
+        if (window.innerWidth < 1000) {
+            if (pageYOffset > 0) {
+                this.$header.classList.add(this.headerClassName);
+            } else {
+                this.$header.classList.remove(this.headerClassName);
+            }
+        } else {
+            if (pageYOffset > 40) {
+                this.$header.classList.add(this.headerClassName);
+            } else {
+                this.$header.classList.remove(this.headerClassName);
+            }
+        }
+
+    }
+}
